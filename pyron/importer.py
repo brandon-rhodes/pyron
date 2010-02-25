3ccaaf932406d88844c30a89da3a4f5c71862372
Brandon Rhodes <brandon@rhodesmill.org>
2010-02-25 13:46:49 -0500

    Wrote an importer hook with which Pyron can make development packages available.

---
+++ pyron/importer.py
@@ -0,0 +1,33 @@
+import imp
+import sys
+
+sys.dont_write_bytecode = True
+
+class PyronLoader(object):
+    """PEP-302 compliant loader for packages being developed with Pyron."""
+
+    def __init__(self, fullname, package_dir, init_path):
+        self.fullname = fullname
+        self.package_dir = package_dir
+        self.init_path = init_path
+
+    def load_module(self, fullname):
+        """Load and return the package."""
+        assert fullname == self.fullname  # PyronFinder called right loader
+        f = open(self.init_path)
+        try:
+            m = imp.load_module(fullname, f, self.init_path, ('.py', 'U', 1))
+            m.__path__ = [ self.package_dir ]
+            return m
+        finally:
+            f.close()
+
+class PyronFinder(object):
+    """PEP-302 compliant finder for packages being developed with Pyron."""
+
+    def __init__(self):
+        self.loaders = []
+
+    def find_module(self, fullname, path=None):
+        """If we have a loader for this package, return it, else None."""
+        return self.loaders.get(fullname, None)
