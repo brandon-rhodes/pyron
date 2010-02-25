664045f1478fe495db8451de9b0c1794ffb4f136
Brandon Rhodes <brandon@rhodesmill.org>
2010-02-25 17:04:46 -0500

    Taught the egg-generation routine how to pull actual files into the zipfile.

--- pyron/eggs.py
+++ pyron/eggs.py
@@ -1,13 +1,39 @@
 """Routine for creating eggs, without creating intermediate files."""
 
+import os
 import pkg_resources
+from fnmatch import fnmatch
 from StringIO import StringIO
 from zipfile import ZipFile, ZipInfo
 
-def create_egg(package_name):
+INCLUDE_PATTERNS = ('*.py', '*.rst', '*.txt')
+
+def append_files(z, dirpath, zipdir):
+    """Append to the zipfile any files found in the given directory."""
+    for filename in sorted(os.listdir(dirpath)):
+        if filename.startswith('.'):
+            continue
+        filepath = os.path.join(dirpath, filename)
+        zippath = (zipdir + '/' + filename) if zipdir else filename
+        if os.path.isdir(filepath):
+            append_files(z, filepath, zippath)
+            continue
+        for pattern in INCLUDE_PATTERNS:
+            if fnmatch(filename, pattern):
+                f = open(filepath)
+                z.writestr(zippath, f.read())
+                f.close()
+                continue
+
+def create_egg(package_name, dir_path):
+    """Return a string containing a zipped egg for the given package."""
+
     f = StringIO()
     z = ZipFile(f, 'w')
-    #z.writestr('EGG-INFO/PKG-INFO', 'foo')
+
+    # Stuff still needed:
+    # requires.txt
+    # entry_points.txt
 
     # Look for periods in the package name to figure out if it is
     # located inside of any namespace packages, and after reducing the
@@ -26,12 +52,18 @@ def create_egg(package_name):
 
     z.writestr('EGG-INFO/not-zip-safe', '\n')
 
+    # Now that we are done with the metadata, save the actual data.
+
+    append_files(z, '.', package_name.replace('.', '/'))
+
     # Finish writing the zipfile data.
 
     z.close()
     return f.getvalue()
 
 def write_egg(name, version, version_info, egg_data):
+    """Write an egg file, formatting its name per the egg specifications."""
+
     filename = '{0}-{1}-py{2[0]}.{2[1]}.egg'.format(
         pkg_resources.to_filename(pkg_resources.safe_name(name)),
         pkg_resources.to_filename(pkg_resources.safe_version(version)),
