bb7e38054620731ea03fd11ec69d5a6122f65079
Brandon Rhodes <brandon@rhodesmill.org>
2010-04-22 12:06:06 -0400

    After learning more about the tarfile module, moved the text of the standard namespace __init__.py back inline in the "sdist" module.

--- pyron/sdist.py
+++ pyron/sdist.py
@@ -2,29 +2,87 @@
 
 import os
 import tarfile
+import time
+from StringIO import StringIO
 
-namespace_init_path = os.path.join(os.path.dirname(__file__),
-                                   'namespace_init.py.txt')
+this_dir = os.path.dirname(__file__)
+namespace_init_path = os.path.join(this_dir, 'namespace_init.py.txt')
+
+NAMESPACE_INIT_PY = """\
+# This is a Python "namespace package" http://www.python.org/dev/peps/pep-0382/
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+"""
+
+SETUP_PY = """\
+# This setup.py was generated automatically by Pyron.
+# For details, see http://pypi.python.org/pypi/pyron/
+
+from distribute import setup
+
+setup(
+    name = %r,
+    
+    )
+"""
+
+def setup_py_text(project):
+    """Return the text of a ``setup.py`` file for `project`."""
+    # setup.py needs:
+    # to know about the package
+    # to know name, version, description, long description
+    # requirements
+    # entry points
+    # author
+    return SETUP_PY % (project.name, )
 
 def package_dir(name):
     """Return the tarfile directory name for a given package."""
     return '/'.join(name.split('.'))
 
+def tar_add(tar, pathparts, text, mtime):
+    """Add a plain-text file to archive `tar`.
+
+    `pathparts` - A list of pathname components like ['foo-0.1', 'setup.py'].
+    `text` - The desired textual content of the file.
+    `mtime` - The modification time that should be recorded for the file.
+
+    """
+    name = '/'.join(pathparts)
+    tarinfo = tarfile.TarInfo(name)
+    tarinfo.size = len(text)
+    tarinfo.mtime = mtime
+    tarinfo.mode = 0644
+    tarinfo.type = tarfile.REGTYPE
+    tarinfo.uid = 65534  # "nobody" on Ubuntu, for what it's worth
+    tarinfo.gid = 65534
+    tarinfo.uname = 'nobody'
+    tarinfo.gname = 'nobody'
+    tar.addfile(tarinfo, StringIO(text))
+
 def write_sdist(project, outfile):
     """Write a gzipped tarfile sdist for `project` to `outfile`."""
 
     tar = tarfile.open(mode='w:gz', fileobj=outfile)
+    now = time.time()
     base = '%s-%s' % (project.name, project.version)
     src = os.path.join(base, 'src')
 
     for np in project.namespace_packages:
-        dirname = '/'.join([ src, package_dir(np) ])
-        tar.add(namespace_init_path, '/'.join([ dirname, '__init__.py' ]))
+        #tarpath = '/'.join([ src, package_dir(np), '__init__.py' ])
+        #tar.add(namespace_init_path, tarpath)
+        pathparts = [ base, 'src' ] + np.split('.') + [ '__init__.py' ]
+        tar_add(tar, pathparts, NAMESPACE_INIT_PY, now)
 
-    dirname = '/'.join([ src, package_dir(project.name) ])
+    pkgbase = '/'.join([ src, package_dir(project.name) ])
     for abspath, relpath in project.find_files():
-        tar.add(abspath,
-                '/'.join([ dirname, relpath ]))
+        tar.add(abspath, '/'.join([ pkgbase, relpath ]))
+
+    #text = setup_py_text(project)
+    #tarinfo = tar.gettarinfo(setup_py_path, '/'.join([ base, 'setup.py' ]))
+    #tarinfo.size = len(text)
+    #tar.addfile(tarinfo, StringIO(text))
+
     tar.close()
 
 def save_temporary_sdist(project, tmpdir):
